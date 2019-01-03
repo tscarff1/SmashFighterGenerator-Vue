247982f1d413d04f6b72564e1586090d8460ac4c
Tyler <tscarff1@gmail.com>
2019-01-03 10:08:44 -0500

    imported new font awesome icon

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ import VueAxios from 'vue-axios'
 import axios from 'axios'
 
 import {library} from '@fortawesome/fontawesome-svg-core'
-import {faInfoCircle, faCog, faList, faRedo} from '@fortawesome/free-solid-svg-icons'
+import {faInfoCircle, faCog, faList, faRedo, faUserAlt} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 Vue.config.productionTip = false
@@ -20,7 +20,7 @@ Vue.use(VueAxios, axios)
 Vue.axios.defaults.baseURL = 'http://localhost:8081/ssbr/api';
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
-library.add(faInfoCircle, faCog, faList, faRedo);
+library.add(faInfoCircle, faCog, faList, faRedo, faUserAlt);
 
 
 new Vue({
