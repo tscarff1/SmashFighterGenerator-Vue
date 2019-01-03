c63882c4821b6a909d5d574d778a4746024ef1da
Tyler <tscarff1@gmail.com>
2019-01-03 10:31:35 -0500

    Filling in Info / credits page

---
+++ src/components/Info.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="info-container">
+        <h3>App built by Tyler Scarff</h3>
+        <p>/u/BroDudeIII on reddit</p>
+        <br />
+        <h3> Character portraits courtesy of /u/NightTide9 on reddit</h3>
+        <p>Follow <a href="https://twitter.com/EvernightStudio">@EvernightStudio</a> on Twitter for more amazing artwork!</p>
+
+        <br />
+        <h3>Smash Bros Ultimate © Nintendo / HAL Laboratory, Inc.</h3>
+        <p>Thanks for such an amazing game!</p>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    .info-container {
+        margin: auto;
+        margin-top: 5vh;
+        width: 80vw;
+        height: 90vh;
+        margin-right: 5vw;
+        background-color: #b3b3b3;
+        color: black;
+        border-style: solid;
+        border-radius: 10px;
+        border-width: 5px;
+        border-color: black;
+    }
+</style>
